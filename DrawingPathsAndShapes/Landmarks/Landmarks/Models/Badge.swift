ca176be428e5addfd5c8c29e30a3e38f17ddf42b
Robin Malhotra <robinm@twitter.com>
2020-04-23 00:38:24 +0100

    Add badge

---
+++ DrawingPathsAndShapes/Landmarks/Landmarks/Models/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Robin Malhotra on 23/04/2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
